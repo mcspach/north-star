f94b511944750cecfa796711cb99607b8b252d2f
Matthew Spach <matthews@anyfans.com>
2023-03-25 20:47:51 -0700

    basic controllers complete with nested routes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,16 +1,40 @@
 class AdminsController < ApplicationController
   def index
+    @admins = Admin.all
   end
 
   def show
+    @admin = Admin.find(params[:id])
   end
 
   def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to @admin
+    else
+      render 'new'
+    end
   end
 
   def update
+    @admin = Admin.find(params[:id])
+    if @admin.update(admin_params)
+      redirect_to @admin
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @admin = Admin.find(params[:id])
+    @admin.destroy
+    redirect_to admins_path
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:name, :email)
   end
 end
+
