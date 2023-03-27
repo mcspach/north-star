70d63e1e509b59bea2fee010c08900e8824c8b34
Matthew Spach <matthews@anyfans.com>
2023-03-27 10:38:59 -0700

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
-  root 'phases#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+    # unlocks: 'users/unlocks',
+    # registrations: 'users/registrations',
+    # passwords: 'users/passwords',
+    # omniauth: 'users/omniauth',
+    # configurations: 'users/configurations'
+    }
+root 'phases#index'
 
-  resources :tasks do
-    post :toggle, on: :member
-  end
+resources :tasks do
+  post :toggle, on: :member
+end
 
-  resources :projects do
-    resources :phases do
-      resources :action_items
-    end
+resources :projects do
+  resources :phases do
+    resources :action_items
   end
+end
 end
