75b37e5a73d4d04ea013fbe031f0f91ea43ece0d
Matthew Spach <matthews@anyfans.com>
2023-04-13 12:42:21 -0700

    switched to tailwind and restyle. new navbar, views, forms.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,45 @@
-//= require jquery3
-//= require popper
-//= require bootstrap
+// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
 
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Burger menus
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('weve got js');
+  // open
+  const burger = document.querySelectorAll('.navbar-burger');
+  const menu = document.querySelectorAll('.navbar-menu');
 
+  if (burger.length && menu.length) {
+    for (var i = 0; i < burger.length; i++) {
+      burger[i].addEventListener('click', function () {
+        for (var j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle('hidden');
+        }
+      });
+    }
+  }
+
+  // close
+  const close = document.querySelectorAll('.navbar-close');
+  const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+  if (close.length) {
+    for (var i = 0; i < close.length; i++) {
+      close[i].addEventListener('click', function () {
+        for (var j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle('hidden');
+        }
+      });
+    }
+  }
+
+  if (backdrop.length) {
+    for (var i = 0; i < backdrop.length; i++) {
+      backdrop[i].addEventListener('click', function () {
+        for (var j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle('hidden');
+        }
+      });
+    }
+  }
+});
